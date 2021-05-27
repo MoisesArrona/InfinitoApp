dd6371f570274b93b40e04f8d208f308183bcfd5
Moises Arrona <57106162+MoisesArrona@users.noreply.github.com>
2021-05-26 23:20:30 -0500

    Protección de rutas Middleware
    Protección a rutas por Middleware

--- routes/web.php
+++ routes/web.php
@@ -12,50 +12,52 @@ Route::get('/', function () {
 Auth::routes();
 
 //Proteger rutas a nivel de rol por middleware
-Route::middleware(['administrador'])->group(function () {
-    Route::get('/administrador', 'HomeController@administrador');
+Route::middleware(['auth'])->group(function () {
+    Route::middleware(['administrador'])->group(function () {
+        Route::get('/administrador', 'HomeController@administrador');
+        
+        Route::resource('/tipo', 'TipoController');
     
-    Route::resource('/tipo', 'TipoController');
-
-    Route::resource('/proveedor', 'ProveedorController');
-
-    Route::resource('/producto', 'ProductoController');
-
-    Route::resource('/equipo', 'EquipoController');
-
-    Route::resource('/empresa', 'EmpresaController');
-
-    Route::resource('/tarea', 'TareaController');
-
-    Route::resource('/reporte', 'ReporteController');
-
-    Route::resource('/empresa', 'EmpresaController');
-
-    Route::resource('/usuario', 'UserController');
-
-    Route::resource('/novedad', 'NovedadController');
-});
-
-Route::middleware(['personal'])->group(function () {
-    Route::get('/personal', 'HomeController@personal');
-
-    Route::resource('/tarea', 'TareaController');
-
-    Route::resource('/novedad', 'NovedadController');
-
-    Route::resource('/reporte', 'ReporteController')->except([
-        'create', 'store'
-    ]);
-});
-
-Route::middleware(['cliente'])->group(function () {
-    Route::get('/cliente', 'HomeController@cliente');
-
-    Route::resource('/reporte', 'ReporteController')->except([
-        'edit', 'update'
-    ]);
-
-    Route::resource('/novedad', 'NovedadController')->only([
-        'index'
-    ]);
+        Route::resource('/proveedor', 'ProveedorController');
+    
+        Route::resource('/producto', 'ProductoController');
+    
+        Route::resource('/equipo', 'EquipoController');
+    
+        Route::resource('/empresa', 'EmpresaController');
+    
+        Route::resource('/tarea', 'TareaController');
+    
+        Route::resource('/reporte', 'ReporteController');
+    
+        Route::resource('/empresa', 'EmpresaController');
+    
+        Route::resource('/usuario', 'UserController');
+    
+        Route::resource('/novedad', 'NovedadController');
+    });
+    
+    Route::middleware(['personal'])->group(function () {
+        Route::get('/personal', 'HomeController@personal');
+    
+        Route::resource('/tarea', 'TareaController');
+    
+        Route::resource('/novedad', 'NovedadController');
+    
+        Route::resource('/reporte', 'ReporteController')->except([
+            'create', 'store'
+        ]);
+    });
+    
+    Route::middleware(['cliente'])->group(function () {
+        Route::get('/cliente', 'HomeController@cliente');
+    
+        Route::resource('/reporte', 'ReporteController')->except([
+            'edit', 'update'
+        ]);
+    
+        Route::resource('/novedad', 'NovedadController')->only([
+            'index'
+        ]);
+    });
 });
