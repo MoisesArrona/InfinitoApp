d951c5e39646b7692f61627439e32eae5b81cfab
Moises Arrona <57106162+moisesarrona@users.noreply.github.com>
2024-02-01 23:15:15 -0600

    Delete routes from others rols

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,7 +3,7 @@
 	<div class="logo-header">
 		
 		<a href="index.html" class="logo">
-			<img src="/assets/img/logoazzara.svg" alt="navbar brand" class="navbar-brand">
+			<h1 class="text-white mt-3">InfinitoApp</h1>
 		</a>
 		<button class="navbar-toggler sidenav-toggler ml-auto" type="button" data-toggle="collapse" data-target="collapse" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon">
